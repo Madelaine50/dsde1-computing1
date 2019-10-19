29044d7a4fd5d4300c48e1c2fb134a88115e4fa6
unknown <mw1419@ic.ac.uk>
2019-10-19 12:51:12 +0100

    if statements

--- Week-2/Numberguess.py
+++ Week-2/Numberguess.py
@@ -2,8 +2,20 @@ print('Guess a number between 1 and 10')
 number = input()
 import random
 dice_roll = random.randint(1,10)
-if (number == dice_roll):
+number = int(number)
+if number != dice_roll:
+    print('guess again')
+    number = input()
+elif number == dice_roll:
     print('congrats')
-else:
-    print('unfortunate')
+    if number == dice_roll
+        print('congrats')
+    elif number != dice_roll
+        print('guess again')
+        number = input(number)
+        if number == dice_roll
+            print('congrats')
+
+
+
     
