f84918d2be9559d9cdc822beee6dae49f7dc37b0
unknown <mw1419@ic.ac.uk>
2019-10-24 14:52:53 +0100

    Period

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,7 +1,11 @@
-def Length(l):
+def Length():
     print('insert length')
     L = input()
 
-def Gravity(g)
+def Gravity():
     print('insert gravity' )
     g = input()
+
+def Period():
+    import math
+     print((2*math.pi)*(math.sqrt(L/g)))
