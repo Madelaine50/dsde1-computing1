c63f2469107cfb48461561165782a99eb53832f2
unknown <mw1419@ic.ac.uk>
2019-10-27 09:52:05 +0000

    period

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,25 +1,21 @@
-def Length(L):
-    try:
-        print('Thank you')
-    except NameError:
-        print('Variable is not defined')
-    except ValueError:
-        print('Equation cannot be solved')
-    
 
-def Gravity(g):
-    try:
-        print('Thank you')
-    except NameError:
-        print('Variable is not defined')
-    except ValueError:
-        print('Equation cannot be solved')
     
-def Period():
+def Period(length, gravity):
     import math
-    print((2*math.pi)*(math.sqrt(x/y))
+#importing math function
+    try:
+        period = (2*math.pi)*(math.sqrt(length / gravity))
 
-Length(input())
-Gravity(input())
-Period()  
+    except TypeError:
+        print("Invalid input(s)")
+
+    except NameError:
+        print("Invalid input(s)")   
+
+    print(period)
+    return period
+
+#Length(2)
+#Gravity(9.81)
+Period(1, 9.81)  
 
